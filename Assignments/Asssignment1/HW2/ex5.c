7067303712b253584b568145d95581ad939ef54c
Reem <roro@gmial.com>
2023-07-16 06:53:14 +0300

    edit EX5

--- Assignments/Asssignment1/HW2/ex5.c
+++ Assignments/Asssignment1/HW2/ex5.c
@@ -3,7 +3,7 @@
 int main ()
 {
 	char c;
-	char arr[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','x'};
+	char arr[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	printf("Enter a character: ");
 	fflush(stdout);
 	scanf("%c",&c);
