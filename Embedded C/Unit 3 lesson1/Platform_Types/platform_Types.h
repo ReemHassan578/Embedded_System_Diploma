e7c63bd695cc65b2f4cb460e2ab961426999b767
Reem <roro@gmial.com>
2023-09-01 22:34:45 +0300

    add platform_types.h

---
+++ Embedded C/Unit 3 lesson1/Platform_Types/platform_Types.h
@@ -0,0 +1,37 @@
+/*
+ * platform_Types.h
+ *
+ *  Created on: Aug 30, 2023
+ *      Author: Reemoo
+ */
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+
+typedef unsigned char       boolean;
+typedef unsigned char  		uint8_t;
+typedef char 				sint8_t;
+typedef unsigned short 		uint16_t;
+typedef short 				sint16_t;
+typedef unsigned int 		uint32_t;
+typedef int 				sint32_t;
+typedef unsigned long long  uint64_t;
+typedef long long 			sint64_t;
+
+typedef volatile unsigned char 		vuint8_t;
+typedef volatile char 				vint8_t;
+
+typedef volatile unsigned short 	vuint16_t;
+typedef volatile short 				vint16_t;
+
+typedef volatile unsigned int	    vuint32_t;
+typedef volatile int 				vint32_t;
+
+typedef volatile unsigned long 		vuint64_t;
+typedef volatile long 				vint64_t;
+
+
+
+
+#endif /* PLATFORM_TYPES_H_ */
